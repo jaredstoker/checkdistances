f26c682ea32bcbc968e80f1a652a22237fd1570c
krisbe <computertechsofamerica@gmail.com>
2014-05-01 17:18:03 -0600

    name changed to make more understand able

--- src/Cites.java
+++ src/Cites.java
@@ -14,26 +14,26 @@ public class Cites {
 		// This should load up the required cities into our graph
 		this.cities = new Vertex[14];
 
-		this.cities[0] = new Vertex<String>("SEA");
-		this.cities[1] = new Vertex<String>("SFR");
-		this.cities[2] = new Vertex<String>("LA");
-		this.cities[3] = new Vertex<String>("LV");
-		this.cities[4] = new Vertex<String>("PHO");
-		this.cities[5] = new Vertex<String>("OKC");
-		this.cities[6] = new Vertex<String>("DAL");
-		this.cities[7] = new Vertex<String>("MIN");
-		this.cities[8] = new Vertex<String>("MIL");
-		this.cities[9] = new Vertex<String>("CHI");
-		this.cities[10] = new Vertex<String>("NOR");
-		this.cities[11] = new Vertex<String>("NYC");
-		this.cities[12] = new Vertex<String>("WDC");
-		this.cities[13] = new Vertex<String>("MIA");
+		this.cities[0] = new city<String>("SEA");
+		this.cities[1] = new city<String>("SFR");
+		this.cities[2] = new city<String>("LA");
+		this.cities[3] = new city<String>("LV");
+		this.cities[4] = new city<String>("PHO");
+		this.cities[5] = new city<String>("OKC");
+		this.cities[6] = new city<String>("DAL");
+		this.cities[7] = new city<String>("MIN");
+		this.cities[8] = new city<String>("MIL");
+		this.cities[9] = new city<String>("CHI");
+		this.cities[10] = new city<String>("NOR");
+		this.cities[11] = new city<String>("NYC");
+		this.cities[12] = new city<String>("WDC");
+		this.cities[13] = new city<String>("MIA");
 
-		this.graph = new WeightedAdjMatrixGraph<>();
+		this.graph = new WeightedAdjMatrixDistance<>();
 
-		for (Vertex<String> city : this.cities)
+		for (city<String> city : this.cities)
 		{
-			this.graph.addVertex(city);
+			this.graph.addcity(city);
 		}
 
 		this.graph.addEdge(this.cities[0], 808, this.cities[1]);
@@ -54,28 +54,28 @@ public class Cites {
 		this.graph.addEdge(this.cities[9], 1423, this.cities[13]);
 		this.graph.addEdge(this.cities[11], 237, this.cities[12]);
 
-		this.connectionGraph = new WeightedAdjMatrixGraph();
-		for (Vertex<String> city : this.cities)
+		this.connectionDistance = new WeightedAdjMatrixDistance();
+		for (city<String> city : this.cities)
 		{
-			this.connectionGraph.addVertex(city);
+			this.connectionDistance.addVertex(city);
 		}
-		this.connectionGraph.addEdge(this.cities[0], this.cities[1]);
-		this.connectionGraph.addEdge(this.cities[0], this.cities[9]);
-		this.connectionGraph.addEdge(this.cities[1], this.cities[2]);
-		this.connectionGraph.addEdge(this.cities[1], this.cities[8]);
-		this.connectionGraph.addEdge(this.cities[2], this.cities[3]);
-		this.connectionGraph.addEdge(this.cities[2], this.cities[6]);
-		this.connectionGraph.addEdge(this.cities[3], this.cities[9]);
-		this.connectionGraph.addEdge(this.cities[4], this.cities[8]);
-		this.connectionGraph.addEdge(this.cities[5], this.cities[7]);
-		this.connectionGraph.addEdge(this.cities[6], this.cities[7]);
-		this.connectionGraph.addEdge(this.cities[6], this.cities[10]);
-		this.connectionGraph.addEdge(this.cities[6], this.cities[11]);
-		this.connectionGraph.addEdge(this.cities[7], this.cities[11]);
-		this.connectionGraph.addEdge(this.cities[8], this.cities[12]);
-		this.connectionGraph.addEdge(this.cities[9], this.cities[10]);
-		this.connectionGraph.addEdge(this.cities[9], this.cities[13]);
-		this.connectionGraph.addEdge(this.cities[11], this.cities[12]);
+		this.connectionDistance.addEdge(this.cities[0], this.cities[1]);
+		this.connectionDistance.addEdge(this.cities[0], this.cities[9]);
+		this.connectionDistance.addEdge(this.cities[1], this.cities[2]);
+		this.connectionDistance.addEdge(this.cities[1], this.cities[8]);
+		this.connectionDistance.addEdge(this.cities[2], this.cities[3]);
+		this.connectionDistance.addEdge(this.cities[2], this.cities[6]);
+		this.connectionDistance.addEdge(this.cities[3], this.cities[9]);
+		this.connectionDistance.addEdge(this.cities[4], this.cities[8]);
+		this.connectionDistance.addEdge(this.cities[5], this.cities[7]);
+		this.connectionDistance.addEdge(this.cities[6], this.cities[7]);
+		this.connectionDistance.addEdge(this.cities[6], this.cities[10]);
+		this.connectionDistance.addEdge(this.cities[6], this.cities[11]);
+		this.connectionDistance.addEdge(this.cities[7], this.cities[11]);
+		this.connectionDistance.addEdge(this.cities[8], this.cities[12]);
+		this.connectionDistance.addEdge(this.cities[9], this.cities[10]);
+		this.connectionDistance.addEdge(this.cities[9], this.cities[13]);
+		this.connectionDistance.addEdge(this.cities[11], this.cities[12]);
 
 	}
 
