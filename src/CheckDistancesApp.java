ab1f31e57172c0d70cc732e8f5cef123194a0b58
Jared Stoker <stokerfam@gmail.com>
2014-05-01 17:24:48 -0600

    GUI Update

--- src/CheckDistancesApp.java
+++ src/CheckDistancesApp.java
@@ -1,3 +1,4 @@
+import javax.swing.JFrame;
 
 public class CheckDistancesApp {
 	public static void main(String[] args)
--- src/CheckDistancesGUI.java
+++ src/CheckDistancesGUI.java
@@ -1,18 +1,20 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.GroupLayout;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JPanel;
+import javax.swing.GroupLayout.Alignment;
 
 
 public class CheckDistancesGUI implements ActionListener{
 		private JRadioButton sea, sfr, la, lv, pho, okc, dal, min, mil, chi, nor, nyc, wdc, mia;
 		private JTextArea output;
-		private JPanel jPanel1;
+		private JPanel jPanel;
 
         private JInternalFrame jInternalFrame;
 		public void actionPerformed(ActionEvent arg0) {
@@ -45,6 +47,55 @@ public class CheckDistancesGUI implements ActionListener{
 	        nyc.setText("New York City");
 	        wdc.setText("Washington DC");
 	        mia.setText("Miami");
+	        
+	        output = new JTextArea();
+	        output.setText("Check the distance");
+	        
+	        sea = new JRadioButton();
+	        sfr = new JRadioButton();
+	        la = new JRadioButton();
+	        lv = new JRadioButton();
+	        pho = new JRadioButton();
+	        okc = new JRadioButton();
+	        dal = new JRadioButton();
+	        min = new JRadioButton();
+	        mil = new JRadioButton();
+	        chi = new JRadioButton();
+	        nor = new JRadioButton();
+	        nyc = new JRadioButton();
+	        wdc = new JRadioButton();
+	        mia = new JRadioButton();
+	        
+	        sea.addActionListener(new java.awt.event.ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e)
+	        	{
+	        		
+	        	}
+	        });
+	        GroupLayout jPanelLayout = new GroupLayout(jPanel);
+	        jPanelLayout.setVerticalGroup(
+	        		jPanelLayout.createParallelGroup(Alignment.LEADING)
+	        		.addGroup(jPanelLayout.createSequentialGroup()
+	        				.addComponent(sea)
+	        				.addComponent(sfr)
+	        				.addComponent(la)
+	        				.addComponent(lv)
+	        				.addComponent(pho)
+	        				.addComponent(okc)
+	        				.addComponent(dal)
+	        				.addComponent(min)
+	        				.addComponent(mil)
+	        				.addComponent(chi)
+	        				.addComponent(nor)
+	        				.addComponent(nyc)
+	        				.addComponent(wdc)
+	        				.addComponent(mia)
+	        				)
+	        		);
+	        
+	        jPanel.setLayout(jPanelLayout);
+	        
 	        
 		}
 }
