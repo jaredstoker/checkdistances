b6aca5225b0660f0f7b41163c1cf2b96883105c7
XanderHarris <alex.connelley@gmail.com>
2014-05-01 22:05:56 -0600

    Added some scaffold code that can be upgraded to
    be a GUI version.

--- src/City.java
+++ src/City.java
@@ -11,13 +11,28 @@ import java.util.ArrayList;
 public class City {
 	private String name;
 	private ArrayList<Distance> distances;
+	private ArrayList<City> connectedCities;
 	
 	public City(String name){
 		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
 	}
 	
 	public void addDistance(Distance distance){
 		distances.add(distance);
 	}
 	
+	public void addConnectedCity(City city){
+		connectedCities.add(city);
+	}
+	
+	public void getConnectedCity(){
+		for(int i = 0; i < connectedCities.size(); i++){
+			City city = connectedCities.get(i);
+			System.out.println(city.getName() + " is connected to " + this.name);
+		}
+	}
 }
